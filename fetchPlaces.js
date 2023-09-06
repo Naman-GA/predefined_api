acf21650ad0644fcb1bb72b01b1bac82c0abbe66
Naman-GA <ngmarch15@gmail.com>
2023-09-06 17:40:11 +0530

    newchangein branch

--- fetchPlaces.js
+++ fetchPlaces.js
@@ -1,37 +1,46 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=search+terms`;
+// const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=search+terms`;
 
-const data = axios
-  .get(apiUrl)
-  .then((response) => {
-    const results = response.data;
+// const data = axios
+//   .get(apiUrl)
+//   .then((response) => {
+//     const results = response.data;
 
-    if (results.items) {
-      results.items.forEach((item) => {
-        const volumeInfo = item.volumeInfo;
+//     if (results.items) {
+//       results.items.forEach((item) => {
+//         const volumeInfo = item.volumeInfo;
 
-        const title = volumeInfo.title;
-        const authors = volumeInfo.authors
-          ? volumeInfo.authors.join(", ")
-          : "Unknown Author";
-        const publishedDate = volumeInfo.publishedDate || "Unknown Date";
-        const bookType = volumeInfo.printType || "Unknown Type";
+//         const title = volumeInfo.title;
+//         const authors = volumeInfo.authors
+//           ? volumeInfo.authors.join(", ")
+//           : "Unknown Author";
+//         const publishedDate = volumeInfo.publishedDate || "Unknown Date";
+//         const bookType = volumeInfo.printType || "Unknown Type";
 
-        console.log(`Title: ${title}`);
-        console.log(`Authors: ${authors}`);
-        console.log(`Published Date: ${publishedDate}`);
-        console.log(`Book Type: ${bookType}`);
-        console.log("---");
-      });
-    } else {
-      console.log("No items found in the response.");
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+//         console.log(`Title: ${title}`);
+//         console.log(`Authors: ${authors}`);
+//         console.log(`Published Date: ${publishedDate}`);
+//         console.log(`Book Type: ${bookType}`);
+//         console.log("---");
+//       });
+//     } else {
+//       console.log("No items found in the response.");
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
 
 function new1() {
   console.log("Hii THis is new added by naman");
 }
+
+function inbranch() {
+  let id = setInterval(() => {
+    console.log("Inside setInterval");
+    clearInterval(id);
+  }, 1000);
+}
+
+inbranch();
